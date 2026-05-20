4d6a72b1f4bd5a39dbb8e430eb7b8b633362a340
keerthirajendran0301-maker <keerthi.rajendran0301@gmail.com>
2026-05-20 16:19:43 +0100

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # Set up the visual layout of the app
-st.set_page_config(page_title='BA Accelerator POC", layout="wide")
+st.set_page_config(page_title="BA Accelerator POC", layout="wide")
 st.title("AI Business nalyst Accelerator")
 st.write ("Convert messy stakeholder notes into a structured Functional Requirements Document (FRD).")
 
